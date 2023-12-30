f46c4344b85f6bc538f08720c369472bcfd62ade
Elie Michel <elie.michel@exppad.com>
2023-12-30 12:16:36 +0100

    Update to chromium/6216

--- cmake/FetchDawn.cmake
+++ cmake/FetchDawn.cmake
@@ -8,14 +8,14 @@ include(FetchContent)
 FetchContent_Declare(
 	dawn
 	#GIT_REPOSITORY https://dawn.googlesource.com/dawn
-	#GIT_TAG        chromium/6108
+	#GIT_TAG        chromium/6216
 	#GIT_SHALLOW ON
 
 	# Manual download mode, even shallower than GIT_SHALLOW ON
 	DOWNLOAD_COMMAND
 		cd ${FETCHCONTENT_BASE_DIR}/dawn-src &&
 		git init &&
-		git fetch --depth=1 https://dawn.googlesource.com/dawn chromium/6108 &&
+		git fetch --depth=1 https://dawn.googlesource.com/dawn chromium/6216 &&
 		git reset --hard FETCH_HEAD
 )
 
