de41a7e6ee1283878a78af094f14ec4a82ff208b
Elie Michel <elie.michel@exppad.com>
2024-03-24 10:59:10 +0100

    Update to v0.19.3.1

--- include/webgpu/wgpu.h
+++ include/webgpu/wgpu.h
@@ -13,6 +13,8 @@ typedef enum WGPUNativeSType {
     WGPUSType_InstanceExtras = 0x00030006,
     WGPUSType_BindGroupEntryExtras = 0x00030007,
     WGPUSType_BindGroupLayoutEntryExtras = 0x00030008,
+    WGPUSType_QuerySetDescriptorExtras = 0x00030009,
+    WGPUSType_SurfaceConfigurationExtras = 0x0003000A,
     WGPUNativeSType_Force32 = 0x7FFFFFFF
 } WGPUNativeSType;
 
@@ -24,6 +26,9 @@ typedef enum WGPUNativeFeature {
     WGPUNativeFeature_VertexWritableStorage = 0x00030005,
     WGPUNativeFeature_TextureBindingArray = 0x00030006,
     WGPUNativeFeature_SampledTextureAndStorageBufferArrayNonUniformIndexing = 0x00030007,
+    WGPUNativeFeature_PipelineStatisticsQuery = 0x00030008,
+    WGPUNativeFeature_StorageResourceBindingArray = 0x00030009,
+    WGPUNativeFeature_PartiallyBoundBindingArray = 0x0003000A,
     WGPUNativeFeature_Force32 = 0x7FFFFFFF
 } WGPUNativeFeature;
 
@@ -77,6 +82,20 @@ typedef enum WGPUGles3MinorVersion {
     WGPUGles3MinorVersion_Force32 = 0x7FFFFFFF
 } WGPUGles3MinorVersion;
 
+typedef enum WGPUPipelineStatisticName {
+    WGPUPipelineStatisticName_VertexShaderInvocations = 0x00000000,
+    WGPUPipelineStatisticName_ClipperInvocations = 0x00000001,
+    WGPUPipelineStatisticName_ClipperPrimitivesOut = 0x00000002,
+    WGPUPipelineStatisticName_FragmentShaderInvocations = 0x00000003,
+    WGPUPipelineStatisticName_ComputeShaderInvocations = 0x00000004,
+    WGPUPipelineStatisticName_Force32 = 0x7FFFFFFF
+} WGPUPipelineStatisticName WGPU_ENUM_ATTRIBUTE;
+
+typedef enum WGPUNativeQueryType {
+    WGPUNativeQueryType_PipelineStatistics = 0x00030000,
+    WGPUNativeQueryType_Force32 = 0x7FFFFFFF
+} WGPUNativeQueryType WGPU_ENUM_ATTRIBUTE;
+
 typedef struct WGPUInstanceExtras {
     WGPUChainedStruct chain;
     WGPUInstanceBackendFlags backends;
@@ -115,8 +134,8 @@ typedef struct WGPUPushConstantRange {
 
 typedef struct WGPUPipelineLayoutExtras {
     WGPUChainedStruct chain;
-    uint32_t pushConstantRangeCount;
-    WGPUPushConstantRange* pushConstantRanges;
+    size_t pushConstantRangeCount;
+    WGPUPushConstantRange const * pushConstantRanges;
 } WGPUPipelineLayoutExtras;
 
 typedef uint64_t WGPUSubmissionIndex;
@@ -139,38 +158,39 @@ typedef struct WGPUShaderModuleGLSLDescriptor {
     WGPUShaderDefine * defines;
 } WGPUShaderModuleGLSLDescriptor;
 
-typedef struct WGPUStorageReport {
-    size_t numOccupied;
-    size_t numVacant;
-    size_t numError;
-    size_t elementSize;
-} WGPUStorageReport;
+typedef struct WGPURegistryReport {
+   size_t numAllocated;
+   size_t numKeptFromUser;
+   size_t numReleasedFromUser;
+   size_t numError;
+   size_t elementSize;
+} WGPURegistryReport;
 
 typedef struct WGPUHubReport {
-    WGPUStorageReport adapters;
-    WGPUStorageReport devices;
-    WGPUStorageReport pipelineLayouts;
-    WGPUStorageReport shaderModules;
-    WGPUStorageReport bindGroupLayouts;
-    WGPUStorageReport bindGroups;
-    WGPUStorageReport commandBuffers;
-    WGPUStorageReport renderBundles;
-    WGPUStorageReport renderPipelines;
-    WGPUStorageReport computePipelines;
-    WGPUStorageReport querySets;
-    WGPUStorageReport buffers;
-    WGPUStorageReport textures;
-    WGPUStorageReport textureViews;
-    WGPUStorageReport samplers;
+    WGPURegistryReport adapters;
+    WGPURegistryReport devices;
+    WGPURegistryReport queues;
+    WGPURegistryReport pipelineLayouts;
+    WGPURegistryReport shaderModules;
+    WGPURegistryReport bindGroupLayouts;
+    WGPURegistryReport bindGroups;
+    WGPURegistryReport commandBuffers;
+    WGPURegistryReport renderBundles;
+    WGPURegistryReport renderPipelines;
+    WGPURegistryReport computePipelines;
+    WGPURegistryReport querySets;
+    WGPURegistryReport buffers;
+    WGPURegistryReport textures;
+    WGPURegistryReport textureViews;
+    WGPURegistryReport samplers;
 } WGPUHubReport;
 
 typedef struct WGPUGlobalReport {
-    WGPUStorageReport surfaces;
+    WGPURegistryReport surfaces;
     WGPUBackendType backendType;
     WGPUHubReport vulkan;
     WGPUHubReport metal;
     WGPUHubReport dx12;
-    WGPUHubReport dx11;
     WGPUHubReport gl;
 } WGPUGlobalReport;
 
@@ -194,6 +214,17 @@ typedef struct WGPUBindGroupLayoutEntryExtras {
     uint32_t count;
 } WGPUBindGroupLayoutEntryExtras;
 
+typedef struct WGPUQuerySetDescriptorExtras {
+    WGPUChainedStruct chain;
+    WGPUPipelineStatisticName const * pipelineStatistics;
+    size_t pipelineStatisticCount;
+} WGPUQuerySetDescriptorExtras WGPU_STRUCTURE_ATTRIBUTE;
+
+typedef struct WGPUSurfaceConfigurationExtras {
+    WGPUChainedStruct chain;
+    WGPUBool desiredMaximumFrameLatency;
+} WGPUSurfaceConfigurationExtras WGPU_STRUCTURE_ATTRIBUTE;
+
 typedef void (*WGPULogCallback)(WGPULogLevel level, char const * message, void * userdata);
 
 #ifdef __cplusplus
@@ -201,12 +232,12 @@ extern "C" {
 #endif
 
 void wgpuGenerateReport(WGPUInstance instance, WGPUGlobalReport * report);
-size_t wgpuInstanceEnumerateAdapters(WGPUInstance instance, WGPUInstanceEnumerateAdapterOptions const * options, WGPUAdapter * adapters);
+size_t wgpuInstanceEnumerateAdapters(WGPUInstance instance, WGPU_NULLABLE WGPUInstanceEnumerateAdapterOptions const * options, WGPUAdapter * adapters);
 
 WGPUSubmissionIndex wgpuQueueSubmitForIndex(WGPUQueue queue, size_t commandCount, WGPUCommandBuffer const * commands);
 
 // Returns true if the queue is empty, or false if there are more queue submissions still in flight.
-WGPUBool wgpuDevicePoll(WGPUDevice device, WGPUBool wait, WGPUWrappedSubmissionIndex const * wrappedSubmissionIndex);
+WGPUBool wgpuDevicePoll(WGPUDevice device, WGPUBool wait, WGPU_NULLABLE WGPUWrappedSubmissionIndex const * wrappedSubmissionIndex);
 
 void wgpuSetLogCallback(WGPULogCallback callback, void * userdata);
 
@@ -214,13 +245,18 @@ void wgpuSetLogLevel(WGPULogLevel level);
 
 uint32_t wgpuGetVersion(void);
 
-void wgpuRenderPassEncoderSetPushConstants(WGPURenderPassEncoder encoder, WGPUShaderStageFlags stages, uint32_t offset, uint32_t sizeBytes, void* const data);
+void wgpuRenderPassEncoderSetPushConstants(WGPURenderPassEncoder encoder, WGPUShaderStageFlags stages, uint32_t offset, uint32_t sizeBytes, void const * data);
 
 void wgpuRenderPassEncoderMultiDrawIndirect(WGPURenderPassEncoder encoder, WGPUBuffer buffer, uint64_t offset, uint32_t count);
 void wgpuRenderPassEncoderMultiDrawIndexedIndirect(WGPURenderPassEncoder encoder, WGPUBuffer buffer, uint64_t offset, uint32_t count);
 
 void wgpuRenderPassEncoderMultiDrawIndirectCount(WGPURenderPassEncoder encoder, WGPUBuffer buffer, uint64_t offset, WGPUBuffer count_buffer, uint64_t count_buffer_offset, uint32_t max_count);
 void wgpuRenderPassEncoderMultiDrawIndexedIndirectCount(WGPURenderPassEncoder encoder, WGPUBuffer buffer, uint64_t offset, WGPUBuffer count_buffer, uint64_t count_buffer_offset, uint32_t max_count);
+
+void wgpuComputePassEncoderBeginPipelineStatisticsQuery(WGPUComputePassEncoder computePassEncoder, WGPUQuerySet querySet, uint32_t queryIndex);
+void wgpuComputePassEncoderEndPipelineStatisticsQuery(WGPUComputePassEncoder computePassEncoder);
+void wgpuRenderPassEncoderBeginPipelineStatisticsQuery(WGPURenderPassEncoder renderPassEncoder, WGPUQuerySet querySet, uint32_t queryIndex);
+void wgpuRenderPassEncoderEndPipelineStatisticsQuery(WGPURenderPassEncoder renderPassEncoder);
 
 #ifdef __cplusplus
 } // extern "C"
